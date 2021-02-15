7549e8553d0b42f9f90a29d15d139503946b0a66
Pavan <pavanpabs@gmail.com>
2021-02-16 02:09:07 +0530

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
 import "./App.css";
 import { Navbar } from "./components";
+import HomePage from "./components/Home/Index";
+
 import ManageMembers from "./pages/ManageMembers";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ManageMembers />
+
+      {/* <HomePage /> */}
     </div>
   );
 }
