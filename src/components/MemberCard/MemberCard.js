fa8200bd51ceac193abe5c1a6543c06891219e94
ushanfernando <ushansankalpafernando@gmail.com>
2021-04-04 23:02:49 +0530

    view member data table inprogress

--- src/components/MemberCard/MemberCard.js
+++ src/components/MemberCard/MemberCard.js
@@ -2,13 +2,25 @@ import React from "react";
 import "./MemberCard.css";
 
 function MemberCard(props) {
+  var icon = "fa-university";
+  if (props.type != null) {
+    if (props.type == "private") {
+      icon = "fa-building";
+    } else if (props.type == "academic") {
+      icon = "fa-graduation-cap";
+    } else if (props.type == "association") {
+      icon = "fa-users";
+    }
+  }
   return (
-    <div className="member-card" data={props.type}>
-      <center>
-        <i className="fas fa-building card-icon"></i>
-        <h3>{props.text}</h3>
-      </center>
-    </div>
+    <a href="/managemembers">
+      <div className="member-card" data={props.type} onClick={"/managemembers"}>
+        <center>
+          <i className={"fas " + icon + " card-icon"}></i>
+          <h3>{props.text}</h3>
+        </center>
+      </div>
+    </a>
   );
 }
 
--- src/pages/ViewMembers.js
+++ src/pages/ViewMembers.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
 import { Table, Container } from "react-bootstrap";
 import { BreadCrum, SideBar, Navbar, AdminCard } from "../components";
 
 function ViewMembers() {
+  const [page, setPage] = useState(1);
+  const [memberList, setMemberList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const tableDATA = memberList.map((p, index) => {
+    return (
+      <tr key={index} >
+        <td>{index + 1}</td>
+        <td></td>
+        <td>{p.name}</td>
+        <td>{p.email}</td>
+        <td>{p.tel}</td>
+        <td>{p.gender}</td>
+        <td>{p.workplace}</td>
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    );
+  });
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/users/register`, {
+      method: "GET",
+      headers: new Headers({
+        Accept: "application/vnd.github.cloak-preview",
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        setMemberList(response);
+        setIsLoading(false);
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  }, [page]);
   return (
     <div className="wrapper">
       <SideBar members={true} viewmembers={true} />
@@ -11,7 +49,7 @@ function ViewMembers() {
         <div class="content">
           <BreadCrum path={["Home", "Users", "View Members"]} />
           <AdminCard title="View Members">
-            <Table striped bordered hover>
+            <table id="example" class="table table-striped table-bordered">
               <thead>
                 <tr>
                   <th>No</th>
@@ -20,25 +58,12 @@ function ViewMembers() {
                   <th>Email</th>
                   <th>Mobile No</th>
                   <th>Gender</th>
-                  <th>Workspace</th>
+                  <th>Workplace</th>
                   <th>Action</th>
                 </tr>
               </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-              </tbody>
-            </Table>
+              <tbody>{tableDATA}</tbody>
+            </table>
           </AdminCard>
         </div>
       </div>
